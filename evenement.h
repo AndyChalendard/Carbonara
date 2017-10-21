a1a6da47a6dd390a4cc7d81dd6564563553a9892
alpeng <alxpg@hotmail.fr>
2017-10-21 18:39:21 +0200

    events a corriger
    corriger mouvements des ennemis

--- evenement.h
+++ evenement.h
@@ -10,6 +10,7 @@
 #define __EVENT_HEADER__
 
 #define SPEED_ENNEMY 1
+#define SPEED_SELF   2
 
 typedef struct{
   int haut;
@@ -24,9 +25,10 @@ void evenementPlay(map_t * map, data_touche * touche, charac_t * player);
 /* une iteration de moveEnnemyTab */
 void moveEnnemy(map_t map, int i);
 void moveEnnemyTab(map_t m);
+void ennemyChangeDir(map_t map, int i);
 
 /* ne verifie la collision que d'un cote */
-int gestionCollision(map_t m, charac_t c, int dir);
+int gestionCollision(map_t map, charac_t * pc, int dir);
 
 void evenement(int * run, SDL_Event * event, data_touche * touche);
 
